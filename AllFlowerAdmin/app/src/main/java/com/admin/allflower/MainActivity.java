e712b2827ebc521d40acc225df23d5213622dd8a
glediaer <glediaer@gmail.com>
2020-11-02 23:16:51 +0900

    [AND-AllFlowerAdmin] 카메라 권한 추가

--- AllFlowerAdmin/app/src/main/java/com/admin/allflower/MainActivity.java
+++ AllFlowerAdmin/app/src/main/java/com/admin/allflower/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
     String[] PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-//            Manifest.permission.CAMERA,
+            Manifest.permission.CAMERA,
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
 //            Manifest.permission.CALL_PHONE
@@ -1290,7 +1290,7 @@ public class MainActivity extends AppCompatActivity {
     private void checkPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 + ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                + ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                + ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
 //                + ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
                 + ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS)
                 + ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -1298,7 +1298,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
 //                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.GET_ACCOUNTS)
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -1315,7 +1315,7 @@ public class MainActivity extends AppCompatActivity {
                                             new String[]{
                                                     Manifest.permission.READ_EXTERNAL_STORAGE,
                                                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                                                    Manifest.permission.CAMERA,
+                                                    Manifest.permission.CAMERA,
 //                                                    Manifest.permission.CALL_PHONE,
                                                     Manifest.permission.GET_ACCOUNTS,
                                                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -1331,7 +1331,7 @@ public class MainActivity extends AppCompatActivity {
                             new String[]{
                                     Manifest.permission.READ_EXTERNAL_STORAGE,
                                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                                    Manifest.permission.CAMERA,
+                                    Manifest.permission.CAMERA,
 //                                    Manifest.permission.CALL_PHONE,
                                     Manifest.permission.GET_ACCOUNTS,
                                     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -1367,7 +1367,7 @@ public class MainActivity extends AppCompatActivity {
                                     new String[]{
                                             Manifest.permission.READ_EXTERNAL_STORAGE,
                                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                                            Manifest.permission.CAMERA,
+                                            Manifest.permission.CAMERA,
                                             Manifest.permission.CALL_PHONE,
                                             Manifest.permission.GET_ACCOUNTS,
                                             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -1387,7 +1387,7 @@ public class MainActivity extends AppCompatActivity {
                                                     new String[]{
                                                             Manifest.permission.READ_EXTERNAL_STORAGE,
                                                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                                                            Manifest.permission.CAMERA,
+                                                            Manifest.permission.CAMERA,
 //                                                            Manifest.permission.CALL_PHONE,
                                                             Manifest.permission.GET_ACCOUNTS,
                                                             Manifest.permission.ACCESS_FINE_LOCATION,
