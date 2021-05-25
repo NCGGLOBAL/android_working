ca4cb6d3f11b924cb50c5db2530ddcf2f0346840
glediaer <glediaer@gmail.com>
2021-05-25 22:05:34 +0900

    [AND-DmMarket] url 변경

--- DmMarket/app/src/main/java/com/admin/dmmarket/common/HNApplication.java
+++ DmMarket/app/src/main/java/com/admin/dmmarket/common/HNApplication.java
@@ -21,8 +21,8 @@ public class HNApplication extends Application {
     private RequestQueue mRequestQueue;
 
     public static boolean mSigned = false;
-    public static String URL = "http://dmmarket.mallup.co.kr";
-    public static String PUSH_URL = "http://dmmarket.mallup.co.kr/m/app/pushRegister.asp";
+    public static String URL = "https://marketdm.co.kr/m";
+    public static String PUSH_URL = URL + "/app/pushRegister.asp";
     public static boolean mIsFirstLoading = false;
     private static HNApplication mInstance;
     public static String mDeviceId = "";
