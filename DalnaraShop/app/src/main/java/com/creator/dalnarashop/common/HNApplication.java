39cf258cf1601901eed6f7726d23eebe478fb671
glediaer <>
2021-09-13 23:45:05 +0900

    [AND-DalnaraShop] 이름, url 변경

--- DalnaraShop/app/src/main/java/com/creator/dalnarashop/common/HNApplication.java
+++ DalnaraShop/app/src/main/java/com/creator/dalnarashop/common/HNApplication.java
@@ -21,7 +21,7 @@ public class HNApplication extends Application {
     private RequestQueue mRequestQueue;
 
     public static boolean mSigned = false;
-    public static String URL = "https://labang.tv";
+    public static String URL = "https://dalnarashop.mallup.co.kr";
     public static String PUSH_URL = URL + "/m/app/pushRegister.asp";
     public static String UPLOAD_URL = URL + "/m/app/";
     public static boolean mIsFirstLoading = false;
