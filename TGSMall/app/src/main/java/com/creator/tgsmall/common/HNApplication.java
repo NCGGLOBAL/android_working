ad80f6648017ee908891990b374c2e274b771840
glediaer <>
2021-12-01 00:39:15 +0900

    [AND-TGSMall] home url 수정

--- TGSMall/app/src/main/java/com/creator/tgsmall/common/HNApplication.java
+++ TGSMall/app/src/main/java/com/creator/tgsmall/common/HNApplication.java
@@ -21,7 +21,7 @@ public class HNApplication extends Application {
     private RequestQueue mRequestQueue;
 
     public static boolean mSigned = false;
-    public static String URL = "http://tgsmall.kr";
+    public static String URL = "https://tgsmall.kr";
     public static String PUSH_URL = URL + "/m/app/pushRegister.asp";
     public static String UPLOAD_URL = URL + "/m/app/";
     public static boolean mIsFirstLoading = false;
