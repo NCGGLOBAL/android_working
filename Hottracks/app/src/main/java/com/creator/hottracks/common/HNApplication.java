4e8f7728316c2fd9f289514475cad6c8ee112764
glediaer <>
2021-12-29 22:06:11 +0900

    [AND-Hottracks] home url 수정

--- Hottracks/app/src/main/java/com/creator/hottracks/common/HNApplication.java
+++ Hottracks/app/src/main/java/com/creator/hottracks/common/HNApplication.java
@@ -21,7 +21,7 @@ public class HNApplication extends Application {
     private RequestQueue mRequestQueue;
 
     public static boolean mSigned = false;
-    public static String URL = "http://hottracks.online";
+    public static String URL = "http://live.hottracks.co.kr";
     public static String PUSH_URL = URL + "/m/app/pushRegister.asp";
     public static String UPLOAD_URL = URL + "/m/app/";
     public static boolean mIsFirstLoading = false;
