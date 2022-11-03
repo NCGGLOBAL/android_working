13e814f7652f84f81a24566402de55d8c8d44600
glediaer <>
2022-11-03 23:32:04 +0900

    [AND-LabangTv] 웹뷰 폰트 사이즈 고정

--- LabangTv/app/src/main/java/com/creator/labangtv/WebViewActivity.java
+++ LabangTv/app/src/main/java/com/creator/labangtv/WebViewActivity.java
@@ -259,6 +259,7 @@ public class WebViewActivity extends Activity {
         mWebView.getSettings().setAppCacheEnabled(true);
         mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         mWebView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        mWebView.getSettings().setTextZoom(100);
 
         mWebView.addJavascriptInterface(new WebAppInterface(this, mWebView), "android");
 
