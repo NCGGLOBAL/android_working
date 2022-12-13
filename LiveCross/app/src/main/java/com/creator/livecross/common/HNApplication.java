77157ee409ee279d2efe0db3bfae1c9a882a27bc
glediaer <>
2022-12-13 20:44:27 +0900

    [AND-LiveCross] home url 변경

--- LiveCross/app/src/main/java/com/creator/livecross/common/HNApplication.java
+++ LiveCross/app/src/main/java/com/creator/livecross/common/HNApplication.java
@@ -22,7 +22,7 @@ public class HNApplication extends Application {
 
 
     public static boolean mSigned = false;
-    public static String URL = "https://livecross.mallup.co.kr";
+    public static String URL = "https://livecross.live";
     public static String PUSH_URL = URL + "/m/app/pushRegister.asp";
     public static String UPLOAD_URL = URL + "/m/app/";
     public static boolean mIsFirstLoading = false;
