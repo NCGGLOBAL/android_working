d9ad95aa7d1403c9e03d7aa29b8cd2d17b9d4b21
glediaer <>
2023-07-13 23:45:20 +0900

    [AND-Bingbing] ACT1012 이미지 업로드 콜백 수정

--- 2023/Bingbing/app/src/main/java/com/creator/bingbing/WebViewActivity.kt
+++ 2023/Bingbing/app/src/main/java/com/creator/bingbing/WebViewActivity.kt
@@ -1309,7 +1309,7 @@ class WebViewActivity : Activity() {
     }
 
     inner class uploadImagesAsyncTask : AsyncTask<String?, Void?, String?>() {
-        var result: String? = ""
+        var result: String? = null
         override fun onPreExecute() {
             super.onPreExecute()
             mProgressDialog = ProgressDialog(mContext)
@@ -1392,7 +1392,10 @@ class WebViewActivity : Activity() {
                 val dialog = builder.create()
                 dialog.show()
             } else {
-                executeJavascript("$mCallback($s)")
+                val jsonObject = JSONObject()
+                jsonObject.put("resCode", "0000")
+                jsonObject.put("resMsg", "정상적으로 등록 되었습니다.")
+                executeJavascript("$mCallback($jsonObject)")
             }
         }
     }
