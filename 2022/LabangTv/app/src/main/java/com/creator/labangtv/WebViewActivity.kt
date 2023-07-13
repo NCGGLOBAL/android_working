f9ba57787f82eb46413b54e486e2470097b129dd
glediaer <>
2023-07-13 23:34:18 +0900

    [AND-LabangTv] ACT1012 이미지 업로드 콜백 수정

--- 2022/LabangTv/app/src/main/java/com/creator/labangtv/WebViewActivity.kt
+++ 2022/LabangTv/app/src/main/java/com/creator/labangtv/WebViewActivity.kt
@@ -1311,7 +1311,7 @@ class WebViewActivity : Activity() {
     }
 
     inner class uploadImagesAsyncTask : AsyncTask<String?, Void?, String?>() {
-        var result: String? = ""
+        var result: String? = null
         override fun onPreExecute() {
             super.onPreExecute()
             mProgressDialog = ProgressDialog(mContext)
@@ -1394,7 +1394,10 @@ class WebViewActivity : Activity() {
                 val dialog = builder.create()
                 dialog.show()
             } else {
-                executeJavascript("$mCallback($s)")
+                val jsonObject = JSONObject()
+                jsonObject.put("resCode", "0000")
+                jsonObject.put("resMsg", "정상적으로 등록 되었습니다.")
+                executeJavascript("$mCallback($jsonObject)")
             }
         }
     }
