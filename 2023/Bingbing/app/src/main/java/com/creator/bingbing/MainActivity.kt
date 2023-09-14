ce42e9f5231aea417441f7ed1226bd80433bd3ce
glediaer <>
2023-09-14 22:13:18 +0900

    [AND-Bingbing] home url 변경, sdk target31 -> 33으로 변경

--- 2023/Bingbing/app/src/main/java/com/creator/bingbing/MainActivity.kt
+++ 2023/Bingbing/app/src/main/java/com/creator/bingbing/MainActivity.kt
@@ -344,9 +344,7 @@ class MainActivity : AppCompatActivity() {
         mWebView!!.settings.domStorageEnabled = true
         mWebView!!.settings.javaScriptCanOpenWindowsAutomatically = true
         mWebView!!.settings.setSupportMultipleWindows(true)
-        mWebView!!.settings.setAppCacheEnabled(true)
         mWebView!!.settings.cacheMode = WebSettings.LOAD_DEFAULT
-        mWebView!!.settings.setAppCachePath(applicationContext.cacheDir.absolutePath)
         mWebView!!.settings.textZoom = 100
         mWebView!!.addJavascriptInterface(WebAppInterface(this, mWebView!!), "android")
         mWebView!!.isDrawingCacheEnabled = true
