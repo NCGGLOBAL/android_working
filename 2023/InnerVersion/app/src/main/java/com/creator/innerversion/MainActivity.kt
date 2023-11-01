67277fff70c557a9c2e6349093333ca404987a24
glediaer <>
2023-11-01 23:30:12 +0900

    [AND-InnerVersion] 네이버 로그인 이슈 수정

--- 2023/InnerVersion/app/src/main/java/com/creator/innerversion/MainActivity.kt
+++ 2023/InnerVersion/app/src/main/java/com/creator/innerversion/MainActivity.kt
@@ -676,6 +676,7 @@ class MainActivity : AppCompatActivity() {
                         || url.contains("kakaopay")
                         || url.contains("naversearchapp://")
                         || url.contains("kakaotalk://")
+                        || url.contains("nidlogin://")
                         || url.contains("http://m.ahnlab.com/kr/site/download"))
             ) {
                 return try {
