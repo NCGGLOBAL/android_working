533e097a60a6ddd3c2cfe57478e573b5d0f5ff9e
glediaer <>
2023-11-17 00:12:21 +0900

    [AND-Bingbing] 푸시 날짜 추가

--- 2023/Bingbing/app/src/main/java/com/creator/bingbing/MyFirebaseMessagingService.kt
+++ 2023/Bingbing/app/src/main/java/com/creator/bingbing/MyFirebaseMessagingService.kt
@@ -192,6 +192,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
                     .setAutoCancel(true)
                     .setSound(defaultSoundUri)
                     .setContentIntent(pendingIntent)
+                    .setShowWhen(true)
                 if (result != null) {
                     notificationBuilder.setStyle(
                         NotificationCompat.BigPictureStyle()
