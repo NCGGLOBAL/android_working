35e21534aebbbf5f47c982a38750c239b0632bcd
glediaer <>
2024-02-01 21:06:22 +0900

    [AND-MallupLive] 푸시 중복 url 이슈 수정

--- 2023/MallupLive/app/src/main/java/com/creator/malluplive/MainActivity.kt
+++ 2023/MallupLive/app/src/main/java/com/creator/malluplive/MainActivity.kt
@@ -75,7 +75,7 @@ class MainActivity : AppCompatActivity() {
     private val mCallbackParam: String? = null
     private var mFirebaseMessaging: FirebaseMessaging? = null
     private var mPushUid: String? = ""
-    private var mLandingUrl = ""
+    private var mLandingUrl: String? = null
     private var mBackPressCloseHandler: BackPressCloseHandler? = null
     private var mIntegrator: IntentIntegrator? = null
     private var mCameraType = 0
@@ -191,32 +191,11 @@ class MainActivity : AppCompatActivity() {
                 sendRegistrationToServer(token)
             }
             LogUtil.e("push token : $token")
-            val intent = intent
-            if (intent.hasExtra("pushUid")) {
-                mPushUid = intent.getStringExtra("pushUid")
-                sendPushReceiveToServer(mPushUid)
-            }
-            if (intent.dataString != null && !intent.dataString!!.isEmpty()) {
-                val landingUri = intent.dataString
-                //                Toast.makeText(this, landingUri, Toast.LENGTH_LONG).show();
-//                Log.e("jj", "landingUri : " + landingUri);
-                var splitUrl = landingUri!!.split("\\?").toTypedArray()[1]
-                //                Log.e("jj", "splitUrl : " + splitUrl);
-                splitUrl = splitUrl.split("=").toTypedArray()[1]
-                //                Log.e("jj", "splitUrl : " + splitUrl);
-                mLandingUrl = splitUrl
-            }
-            //            Log.e("jj", "mLandingUrl : " + mLandingUrl);
+            mPushUid = intent.getStringExtra("pushUid")
+            mLandingUrl = intent.getStringExtra("url")
+            LogUtil.e("mPushUid : $mPushUid")
+            LogUtil.e("mLandingUrl : $mLandingUrl")
 
-//            if (intent != null) {
-//                if (intent.hasExtra("pushUid") && intent.hasExtra("url")) {
-//                    if (!intent.getStringExtra("url").equals("")) {
-//                        mPushUid = intent.getStringExtra("pushUid");
-//                        mLandingUrl = intent.getStringExtra("url");
-//                        sendPushReceiveToServer(mPushUid);
-//                    }
-//                }
-//            }
 
             // permission 체크 - 최초실행
             if (HNSharedPreference.getSharedPreference(
@@ -260,6 +239,19 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         mWebView!!.onPause()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            mLandingUrl = intent.getStringExtra("url")
+            Log.e(TAG, "mLandingUrl : $mLandingUrl")
+            val extraHeaders: MutableMap<String, String> = HashMap()
+            extraHeaders["webview-type"] = "main"
+            mLandingUrl?.let {
+                mWebView?.loadUrl(it, extraHeaders)
+            }
+        }
     }
 
     private val hashKey: Unit
@@ -352,11 +344,13 @@ class MainActivity : AppCompatActivity() {
         mWebView!!.buildDrawingCache()
         val extraHeaders: MutableMap<String, String> = HashMap()
         extraHeaders["webview-type"] = "main"
-        if (mLandingUrl != "") {
-            mWebView!!.loadUrl(mLandingUrl, extraHeaders)
-        } else {
-            mWebView!!.loadUrl(HNApplication.URL, extraHeaders)
-            mLandingUrl = ""
+        mWebView?.loadUrl(HNApplication.URL, extraHeaders)
+        mLandingUrl?.let {
+            intent = Intent(mContext, WebViewActivity::class.java)
+            intent.putExtra("webviewUrl", it)
+            startActivity(intent)
+
+            mLandingUrl = null
         }
     }
 
