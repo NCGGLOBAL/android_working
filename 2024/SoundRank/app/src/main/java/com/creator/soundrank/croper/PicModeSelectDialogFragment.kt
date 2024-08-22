926c94a0c81d1448c0ed226e1fbd684a3e73e948
glediaer <>
2024-08-22 22:04:32 +0900

    [AND-SoundRank] 프로젝트 생성

---
+++ 2024/SoundRank/app/src/main/java/com/creator/soundrank/croper/PicModeSelectDialogFragment.kt
@@ -0,0 +1,33 @@
+package com.creator.soundrank.croper
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
+import com.creator.soundrank.croper.GOTOConstants.PicModes
+
+/**
+ * @author GT
+ */
+class PicModeSelectDialogFragment : DialogFragment() {
+    private val picMode = arrayOf<String>(PicModes.Companion.CAMERA, PicModes.Companion.GALLERY)
+    private var iPicModeSelectListener: IPicModeSelectListener? = null
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Select Mode")
+            .setItems(picMode) { dialog, which ->
+                if (iPicModeSelectListener != null) iPicModeSelectListener!!.onPicModeSelected(
+                    picMode[which]
+                )
+            }
+        return builder.create()
+    }
+
+    fun setiPicModeSelectListener(iPicModeSelectListener: IPicModeSelectListener?) {
+        this.iPicModeSelectListener = iPicModeSelectListener
+    }
+
+    interface IPicModeSelectListener {
+        fun onPicModeSelected(mode: String)
+    }
+}
---
+++ 2024/SoundRank/app/src/main/java/com/creator/soundrank/delegator/HNCommTranInterface.kt
@@ -0,0 +1,8 @@
+package com.creator.soundrank.delegator
+
+/**
+ * Created by skcrackers on 5/25/16.
+ */
+interface HNCommTranInterface {
+    fun recvMsg(tranCode: String?, params: String)
+}
