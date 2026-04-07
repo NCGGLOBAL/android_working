9cbebe29f2f5b268e1f325b21523a5569893e9e2
glediaer <glediaer@gmail.com>
2026-04-07 20:41:00 +0900

    [AND-Gogorealbox] 신규 프로젝트 생성

---
+++ 2026/Gogorealbox/app/src/main/java/com/creator/gogorealbox/delegator/HNPlugun.kt
@@ -0,0 +1,3 @@
+package com.creator.gogorealbox.delegator
+
+open class HNPlugun
---
+++ 2026/Gogorealbox/app/src/main/java/com/creator/gogorealbox/models/Album.kt
@@ -0,0 +1,6 @@
+package com.creator.gogorealbox.models
+
+/**
+ * Created by Darshan on 4/14/2015.
+ */
+class Album(var name: String, var cover: String)
